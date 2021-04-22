71184997b7804598a6ec6acb4151ac846fc37d3c
Phuc Hoang <hvphuc081299@gmail.com>
2021-04-22 16:17:04 +0700

    add provider redux in app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import 'App.scss';
-import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import Login from 'features/Auth/pages/Login';
 import NotFound from 'components/NotFound';
 import PublicRoute from 'components/PublicRoute';
 import PrivateRoute from 'components/PrivateRoute';
 import User from 'features/User';
+import { Provider } from 'react-redux';
+import store from 'app/store';
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Switch>
-				<PublicRoute path="/login" component={Login} />
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <PublicRoute path="/login" component={Login} />
 
-				<PrivateRoute path="/users" component={User} />
+          <PrivateRoute path="/users" component={User} />
 
-				<Redirect exact from="/" to="/users" />
-				<Route component={NotFound} />
-			</Switch>
-		</BrowserRouter>
-	)
+          <Redirect exact from="/" to="/users" />
+          <Route component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
 export default App;
