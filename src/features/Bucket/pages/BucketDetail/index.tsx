d12721a46580d9a694f940aa191ea5e2cf5b2384
khanhhongtranle <khanhhong141049@gmail.com>
2021-05-01 00:14:29 +0700

    Bucket detail page & Object table component

--- src/features/Bucket/pages/BucketDetail/index.tsx
+++ src/features/Bucket/pages/BucketDetail/index.tsx
@@ -1,7 +1,132 @@
-import React from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
+import HeaderPage from "components/HeaderPage";
+import {Button, Input, message, TablePaginationConfig} from "antd";
+import {SearchOutlined, DownloadOutlined, UploadOutlined} from "@ant-design/icons";
+import ObjectTable, {IObjectRow} from "features/Object/components/ObjectTable";
+import {range} from "lodash";
+import {FilterValue, SorterResult} from "antd/lib/table/interface";
+import {useHistory} from "react-router";
+
+const dummyData = range(0, 30, 1).map((index: number) => ({
+  id: index,
+  name: `Object name ${index}`,
+  folder: `Folder ${index}`,
+  type: `txt`,
+  size: `${index} MB`,
+  dateModified: '30/04/2021',
+}));
 
 function BucketDetail(): JSX.Element {
-  return <div>Bucket Detail Page</div>;
+  const history = useHistory();
+  const [loading, setLoading] = useState<boolean>(true);
+  const [selectedObjectKeys, setSelectedObjectKeys] = useState<React.Key[]>([]);
+
+  //Search
+  let timeout: NodeJS.Timeout;
+  useEffect(() => {
+    //Todo: Call api to get bucket list
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = e;
+    if (timeout) {
+      clearTimeout(timeout);
+    }
+    timeout = setTimeout(() => {
+      message.info(`Searching with ${value}`);
+    }, 500);
+  };
+
+  //Table
+  const handleSelect = (selectedRowKeys: React.Key[], selectedRows: IObjectRow[]): void => {
+    console.log('🚀 ~ file: index.tsx ~ line 58 ~ handleChangeSelect ~ selectedRows', selectedRows);
+    console.log('🚀 ~ file: index.tsx ~ line 58 ~ handleChangeSelect ~ selectedRowKeys', selectedRowKeys);
+    setSelectedObjectKeys(selectedRowKeys);
+  };
+
+  const handleChange = (
+    pagination: TablePaginationConfig,
+    filter: Record<string, FilterValue | null>,
+    sorter: SorterResult<IObjectRow> | SorterResult<IObjectRow>[],
+  ): void => {
+    console.log('🚀 ~ file: index.tsx ~ line 72 ~ handleChange ~ pagination', pagination);
+    console.log('🚀 ~ file: index.tsx ~ line 72 ~ handleChange ~ sorter', sorter);
+  };
+
+  const handleDeleteMulObjects = () => {
+    if (!selectedObjectKeys.length) {
+      message.warning('Please select at least one object');
+      return;
+    }
+    console.log('Delete: ');
+    console.log({ selectedObjectKeys });
+    message.info('Delete multi objects');
+  };
+
+  const handleViewObject = (bucketID: number, objectID: number): void => {
+    history.push(`/buckets/${bucketID}/${objectID}`);
+  };
+
+  const handleDeleteObject = (bucketID: number, objectID: number): void => {
+    message.info(`Delete object id = ${objectID}`);
+  };
+    
+  return (
+    <>
+      <HeaderPage
+        title="Bucket name"
+        breadcrumbs={[
+          {
+            label: 'Back to buckets list',
+            path: `/buckets/`,
+          },
+        ]}
+      />
+      <div className="bucket-table-container">
+        <div className="d-flex justify-content-between">
+          <div className="bucket-table-container__search">
+            <Input size="large" placeholder="Search..." prefix={<SearchOutlined />} />
+          </div>
+          <div className="bucket-table-container__actions">
+            <Button type="primary" icon={<UploadOutlined/>}>
+                            Upload
+            </Button>
+            <Button className="ml-2" type="default" icon={<DownloadOutlined />}>
+                  Download
+            </Button>
+            <Button className="ml-2" type="default" >
+                  Create folder
+            </Button>
+            <Button className="ml-2" type="primary" danger onClick={handleDeleteMulObjects}>
+                  Delete
+            </Button>
+          </div>
+        </div>
+        <div className="mt-4">
+          <ObjectTable
+            loading={loading}
+            data={dummyData}
+            onChange={handleChange}
+            onSelect={handleSelect}
+            onSearch={handleSearch}
+            onViewDetail={handleViewObject}
+            onDelete={handleDeleteObject}
+          />
+        </div>
+        {/*<ModalCreateBucket*/}
+        {/*  visible={visibleModalCreate}*/}
+        {/*  onCancel={toggleModalCreate}*/}
+        {/*  onOk={handleCreateBucket}*/}
+        {/*  loading={loading}*/}
+        {/*  form={createBucketForm}*/}
+        {/*/>*/}
+      </div>
+    </>
+  );
 }
 
 export default BucketDetail;
---
+++ src/features/Object/components/ObjectTable/index.tsx
@@ -0,0 +1,81 @@
+import { Table, TablePaginationConfig } from 'antd';
+import { FilterValue, SorterResult } from 'antd/lib/table/interface';
+import React, { ChangeEvent } from 'react';
+
+export interface IObjectRow {
+  id: number;
+  name: string;
+  folder: string;
+  type: string;
+  size: string;
+  dateModified: string;
+}
+
+interface IObjectTable {
+  loading: boolean;
+  data: IObjectRow[];
+  onSearch: (e: ChangeEvent<HTMLInputElement>) => void;
+  onChange: (
+    pagination: TablePaginationConfig,
+    filter: Record<string, FilterValue | null>,
+    sorter: SorterResult<IObjectRow> | SorterResult<IObjectRow>[],
+  ) => void;
+  onSelect: (selectedRowKeys: React.Key[], selectedRows: IObjectRow[]) => void;
+  onViewDetail: (bucketID: number, objectID: number) => void;
+  onDelete: (bucketID: number, objectID: number) => void;
+}
+
+function BucketTable(props: IObjectTable): JSX.Element {
+  const { loading = true, data = [], onChange, onSelect, onViewDetail, onDelete } = props;
+
+  const columns = [
+    {
+      title: 'Object name',
+      dataIndex: 'name',
+      fixed: true,
+      sorter: true,
+      sortDirection: ['descend', 'ascend'],
+    },
+    {
+      title: 'Folder',
+      dataIndex: 'folder',
+      sorter: true,
+      sortDirection: ['descend', 'ascend'],
+      width: '150px',
+    },
+    {
+      title: 'Type',
+      dataIndex: 'type',
+      sortDirection: ['descend', 'ascend'],
+      sorter: true,
+    },
+    {
+      title: 'Size',
+      dataIndex: 'size',
+      sortDirection: ['descend', 'ascend'],
+      sorter: true,
+    },
+    {
+      title: 'Date modified',
+      dataIndex: 'dateModified',
+      sortDirection: ['descend', 'ascend'],
+      sorter: true,
+    },
+  ];
+
+  return (
+    <Table
+      rowKey="id"
+      className="bucket-table-container__table"
+      loading={loading}
+      columns={columns}
+      dataSource={data}
+      rowSelection={{ type: 'checkbox', onChange: onSelect }}
+      scroll={{ x: 1000, y: 400 }}
+      pagination={{ pageSize: 10 }}
+      onChange={onChange}
+    />
+  );
+}
+
+export default BucketTable;
