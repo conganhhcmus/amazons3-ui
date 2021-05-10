0eee67a073d5099d2e0b31b2626e680427bbf2b5
boy2balls <nguyendanghoaa@gmail.com>
2021-05-10 13:41:51 +0700

    sort user

--- src/app/userlist/userliststore.ts
+++ src/app/userlist/userliststore.ts
@@ -1,15 +1,222 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+export interface fake {
+  userName: string;
+  permisstion: string;
+  accessKeyAge: string;
+  passwordAge: string;
+  lastAcctivity: string;
+}
+
+const fake1: fake[] = [
+  {
+    userName: '921',
+    permisstion: 'fullaccess',
+    accessKeyAge: '20 day ago',
+    passwordAge: '15 day ago',
+    lastAcctivity: '15 day ago',
+  },
+  {
+    userName: 'boy2ball',
+    permisstion: 'fullaccess',
+    accessKeyAge: '1 day ago',
+    passwordAge: '1 day ago',
+    lastAcctivity: '1 day ago',
+  },
+  {
+    userName: 'boy1balls',
+    permisstion: 'noaccess',
+    accessKeyAge: '13 day ago',
+    passwordAge: '12 day ago',
+    lastAcctivity: '16 day ago',
+  },
+  {
+    userName: 'boy41ball',
+    permisstion: 'writeonly',
+    accessKeyAge: '1 day ago',
+    passwordAge: '1 day ago',
+    lastAcctivity: '1 day ago',
+  },
+  {
+    userName: 'boy21ball',
+    permisstion: 'readonly',
+    accessKeyAge: '13 day ago',
+    passwordAge: '17 day ago',
+    lastAcctivity: '10 day ago',
+  },
+  {
+    userName: 'boy61ball',
+    permisstion: 'fullaccess',
+    accessKeyAge: '11 day ago',
+    passwordAge: '12 day ago',
+    lastAcctivity: '13 day ago',
+  },
+  {
+    userName: 'boy31ball',
+    permisstion: 'fullaccess',
+    accessKeyAge: '13 day ago',
+    passwordAge: '12 day ago',
+    lastAcctivity: '17 day ago',
+  },
+  {
+    userName: 'boy51ball',
+    permisstion: 'fullaccess',
+    accessKeyAge: '18 day ago',
+    passwordAge: '16 day ago',
+    lastAcctivity: '14 day ago',
+  },
+  {
+    userName: 'boy01ball',
+    permisstion: 'fullaccess',
+    accessKeyAge: '21 day ago',
+    passwordAge: '31 day ago',
+    lastAcctivity: '41 day ago',
+  },
+  {
+    userName: 'boy15ball',
+    permisstion: 'fullaccess',
+    accessKeyAge: '15 day ago',
+    passwordAge: '15 day ago',
+    lastAcctivity: '15 day ago',
+  },
+  {
+    userName: '921',
+    permisstion: 'fullaccess',
+    accessKeyAge: '15 day ago',
+    passwordAge: '15 day ago',
+    lastAcctivity: '15 day ago',
+  },
+  {
+    userName: '921',
+    permisstion: 'fullaccess',
+    accessKeyAge: '20 day ago',
+    passwordAge: '15 day ago',
+    lastAcctivity: '15 day ago',
+  },
+]
 const initialState = {
   createIamUser: {
     userName: '',
     passWord: '',
     permisstion: '',
   },
-};
+  listUser: fake1,
+}
+const sortIncrease=(data: Array<fake>, key: number)=>{
+  switch(key){
+  case 1: 
+    data.sort((a,b)=>{
+      if(a.userName<b.userName) 
+        return -1
+      if(a.userName>b.userName) 
+        return 1
+      return 0
+    })   
+    break
+  case 2:
+    data.sort((a,b)=>{
+      if(a.permisstion<b.permisstion) return -1
+      if(a.permisstion>b.permisstion) return 1
+      return 0
+    })
+    break
+  case 3:
+    data.sort((a,b)=>{
+      if(a.accessKeyAge<b.accessKeyAge) return -1
+      if(a.accessKeyAge>b.accessKeyAge) return 1
+      return 0
+    })
+    break
+  case 4:
+    data.sort((a,b)=>{
+      if(a.passwordAge<b.passwordAge) return -1
+      if(a.passwordAge>b.passwordAge) return 1
+      return 0
+    })
+    break
+  case 5:
+    data.sort((a,b)=>{
+      if(a.lastAcctivity<b.lastAcctivity) return -1
+      if(a.lastAcctivity>b.lastAcctivity) return 1
+      return 0
+    })
+    break
+  }
+  return data
+}
+const sortDecrease=(data: Array<fake>, key: number)=>{
+  sortIncrease(data,key).reverse()
+}
+const handleSort =(data: Array<fake>, key: number)=>{
+  data=data.concat()
+  if(data.length>1){
+    console.log('key',key)
+    if(key==1){
+      for(let i=1; i< data.length;i++){
+        if(data[0].userName !=data[i].userName && data[0].userName> data[i].userName){
+          sortIncrease(data,key)
+          break
+        }
+        if(data[0].userName !=data[i].userName && data[0].userName< data[i].userName){
+          sortDecrease(data,key)
+          break
+        }
+      }
+    }
+    if(key==2){
+      for(let i=1; i< data.length;i++){
+        if(data[0].permisstion !=data[i].permisstion && data[0].permisstion> data[i].permisstion){
+          sortIncrease(data,key)
+          break
+        }
+        if(data[0].permisstion !=data[i].permisstion && data[0].permisstion< data[i].permisstion){
+          sortDecrease(data,key)
+          break
+        }
+      }
+    }
+    if(key==3){
+      for(let i=1; i< data.length;i++){
+        if(data[0].accessKeyAge !=data[i].accessKeyAge && data[0].accessKeyAge> data[i].accessKeyAge){
+          sortIncrease(data,key)
+          break
+        }
+        if(data[0].accessKeyAge !=data[i].accessKeyAge && data[0].accessKeyAge< data[i].accessKeyAge){
+          sortDecrease(data,key)
+          break
+        }
+      }
+    } 
+    if(key==4){
+      for(let i=1; i< data.length;i++){
+        if(data[0].passwordAge !=data[i].passwordAge && data[0].passwordAge> data[i].passwordAge){
+          sortIncrease(data,key)
+          break
+        }
+        if(data[0].passwordAge !=data[i].passwordAge && data[0].passwordAge< data[i].passwordAge){
+          sortDecrease(data,key)
+          break
+        }
+      }
+    }
+    else{
+      for(let i=1; i< data.length;i++){
+        if(data[0].lastAcctivity !=data[i].lastAcctivity && data[0].lastAcctivity> data[i].lastAcctivity){
+          sortIncrease(data,key)
+          break
+        }
+        if(data[0].lastAcctivity !=data[i].lastAcctivity && data[0].lastAcctivity< data[i].lastAcctivity){
+          sortDecrease(data,key)
+          break
+        }
+      }
+    }       
 
+  }
+  return data
+}
 const userListSlice = createSlice({
-  name: 'createIamUser',
+  name: 'userlist',
   initialState,
   reducers: {
     createIamUser: (state, action) => {
@@ -18,9 +225,12 @@ const userListSlice = createSlice({
         ...action.payload,
       };
     },
+    sortUser: (state, action) => {
+      state.listUser= handleSort(state.listUser, action.payload)
+    },
   },
 });
 
 const { reducer, actions } = userListSlice;
-export const { createIamUser } = actions;
+export const { createIamUser, sortUser } = actions;
 export default reducer;
--- src/features/User/pages/UserList/index.tsx
+++ src/features/User/pages/UserList/index.tsx
@@ -4,41 +4,63 @@ import {  Input } from 'antd';
 import './userlist.css'
 import { Button as Button1} from 'antd';
 import { Button, Modal,Pagination,Dropdown, Icon } from 'semantic-ui-react'
-import { useDispatch } from 'react-redux';
-import { createIamUser } from 'app/userlist/userliststore'
-function UserList(): JSX.Element {
+import { useDispatch,connect } from 'react-redux';
+import { createIamUser,sortUser } from 'app/userlist/userliststore'
+import {fake} from 'app/userlist/userliststore'
+interface Ioption{
+  key: string,
+  value: string,
+  text: string
+}
+interface Iuserstate{
+  user: {
+    token: string
+  },
+  userlistReducer:{
+    createIamUser:{
+      userName: string,
+      passWord: string,
+      permisstion: string
+    },
+    listUser:fake[]
+  }
+}
+
+interface Iuserlist {
+  fake1:fake[]
+}
+const Options: Ioption[]=[
+  {
+    key: 'fullaccess',
+    value:'fullaccess',
+    text: 'Full Access'
+  },
+  {
+    key: 'readonly',
+    value:'readonly',
+    text: 'Read Only'
+  },
+  {
+    key: 'writeonly',
+    value:'writeonly',
+    text: 'Write Only'
+  },
+  {
+    key: 'noaccess',
+    value:'noaccess',
+    text: 'No Access'
+  },
+]
+function UserList(props:Iuserlist): JSX.Element {
   const [open, setOpen] = React.useState<boolean>(false)
-  const [currentPage,setCurrentPage]= React.useState<any>(1)
+  const [currentPage,setCurrentPage]= React.useState<number>(1)
   const dispatch = useDispatch()
-  const Options =[
-    {
-      key: 'fullaccess',
-      value:'fullaccess',
-      text: 'Full Access'
-    },
-    {
-      key: 'readonly',
-      value:'readonly',
-      text: 'Read Only'
-    },
-    {
-      key: 'writeonly',
-      value:'writeonly',
-      text: 'Write Only'
-    },
-    {
-      key: 'noaccess',
-      value:'noaccess',
-      text: 'No Access'
-    },
-  ]
+  const {fake1}=props
   const itemperPage= 3
-  const totalPage= Math.ceil(Options.length/itemperPage)
+  const totalPage= Math.ceil(fake1.length/itemperPage)
   const start= itemperPage * (currentPage-1)
   const end= start+ itemperPage
-  // const handleSort =(sort:Array<string>)=>{
-  //   console.log('asd',sort)
-  // }
+
   return(
     <div>
       <div>User List Page</div>
@@ -102,32 +124,32 @@ function UserList(): JSX.Element {
             <div className='ctit'>
               <div className='user__body__text'>User Name</div>
               <Icon name='sort'  className='sort__icon' 
-                // onClick={()=>handleSort(Options)}
+                onClick={()=>dispatch(sortUser(1))}
               />
             </div>    
           </div>
           <div className='ctit' style={{ width: '18%', justifyContent: 'flex-start'}}>
             <div className='user__body__text'>Permisstion</div>
             <Icon name='sort' className='sort__icon' 
-              // onClick={handleSort(Options)}
+              onClick={()=>dispatch(sortUser(2))}
             />
           </div>
           <div className='ctit' style={{ width: '15%', justifyContent: 'flex-start'}}>
             <div className='user__body__text'>Access key age</div>
             <Icon name='sort' className='sort__icon' 
-              // onClick={handleSort} 
+              onClick={()=>dispatch(sortUser(3))}
             /> 
           </div>
           <div className='ctit' style={{ width: '15%', justifyContent: 'flex-start'}}>
             <div className='user__body__text'>Password age</div>
             <Icon name='sort' className='sort__icon' 
-              // onClick={handleSort} 
+              onClick={()=>dispatch(sortUser(4))}
             />
           </div>
           <div className='ctit' style={{ width: '15%', justifyContent: 'flex-start'}}>
             <div className='user__body__text'>Last activity</div>
             <Icon name='sort' className='sort__icon' 
-              // onClick={handleSort}
+              onClick={()=>dispatch(sortUser(5))}
             />
           </div>
           <div className='ctit'>
@@ -135,37 +157,37 @@ function UserList(): JSX.Element {
           </div>
         </div>
       </div>
-      {Options.slice(start,end).map((e,i)=>{
+      {fake1 !=null&&fake1.slice(start,end).map((e,i)=>{
         return(
           <div className='user__body user__item' key={i}>
             <div className='bt' style={{ paddingBottom: '10px', width: '100%'}}>
               <div className='ctit' style={{ width: '23%', justifyContent: 'flex-start'}}>
                 <input type='checkbox' style={{ marginRight: '30px'}}></input>
                 <div className='ctit'>
-                  <div className='user__body__text2'>{e.text}</div>
+                  <div className='user__body__text2'>{e.userName}</div>
                 </div>    
               </div>
               <div className='ctit' style={{ width: '18%', justifyContent: 'flex-start'}}>
                 <div className='user__body__text2'>
-                  <Button1  className={(e.key=='fullaccess')?'user__item__btn fullaccess':
-                    ((e.key=='readonly')?'user__item__btn readonly':
-                      (e.key=='writeonly')?'user__item__btn writeonly':'user__item__btn noaccess'
+                  <Button1  className={(e.permisstion=='fullaccess')?'user__item__btn fullaccess':
+                    ((e.permisstion=='readonly')?'user__item__btn readonly':
+                      (e.permisstion=='writeonly')?'user__item__btn writeonly':'user__item__btn noaccess'
                     )}>
-                    {e.text}
+                    {e.permisstion}
                   </Button1>
                 </div>  
               </div>
               <div className='ctit' style={{ width: '15%', justifyContent: 'flex-start'}}>
                 <div className='ctit'>
                   <CheckCircleTwoTone twoToneColor="#52c41a" style={{ fontSize: '25px'}}/>
-                  <div className='user__body__text2' style={{ marginLeft: '5px' }}>4 days ago</div>   
+                  <div className='user__body__text2' style={{ marginLeft: '5px' }}>{e.accessKeyAge}</div>   
                 </div>
               </div>
               <div className='ctit' style={{ width: '15%', justifyContent: 'flex-start'}}>
-                <div className='user__body__text2'>4 days ago</div>   
+                <div className='user__body__text2'>{e.passwordAge}</div>   
               </div>
               <div className='ctit' style={{ width: '15%', justifyContent: 'flex-start'}}>
-                <div className='user__body__text2'>4 days ago</div>   
+                <div className='user__body__text2'>{e.lastAcctivity}</div>   
               </div>
               <div className='ctit'>
                 <div>
@@ -183,7 +205,7 @@ function UserList(): JSX.Element {
           defaultActivePage={1}
           ellipsisItem={null}
           firstItem={null}
-          onPageChange={(e,data)=>setCurrentPage(data.activePage)}
+          onPageChange={(e,data)=>setCurrentPage(data.activePage as number)}
           lastItem={null}
           siblingRange={1}
           totalPages={totalPage}
@@ -192,7 +214,8 @@ function UserList(): JSX.Element {
 
     </div>
   ) 
-
 }
-
-export default UserList;
+const mapStatetoProps= (state: Iuserstate)=>({
+  fake1:state.userlistReducer.listUser
+})
+export default connect(mapStatetoProps,null)(UserList);
