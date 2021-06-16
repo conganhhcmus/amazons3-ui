70b6b116b64c62eec7f525104859cdcc65bbbb74
Phuc Hoang <hvphuc081299@gmail.com>
2021-06-16 11:16:04 +0700

    change base url

--- src/api/bucketApi.ts
+++ src/api/bucketApi.ts
@@ -1,13 +1,15 @@
 import { IBucket } from './../features/Bucket/pages/BucketList/index';
 import axiosClient from './axiosClient';
 
+const baseUrl = 'https://storage-api-app.herokuapp.com/';
+
 const bucketApi = {
   getBuckets: (): Promise<{ data: IBucket[] }> => {
-    const url = 'https://storage-api-app.herokuapp.com/api/v1/buckets';
+    const url = `${baseUrl}api/v1/buckets`;
     return axiosClient.get(url);
   },
   createBucket: (bucketName: string, region: string, user_id: string): any => {
-    const url = 'https://storage-api-app.herokuapp.com/api/v1/buckets';
+    const url = `${baseUrl}api/v1/buckets`;
     const body = {
       name: bucketName,
       region,
@@ -17,7 +19,7 @@ const bucketApi = {
     return axiosClient.post(url, body);
   },
   deleteBucket: (id: string): Promise<{ message: string }> => {
-    const url = `https://storage-api-app.herokuapp.com/api/v1/buckets/${id}`;
+    const url = `${baseUrl}api/v1/buckets/${id}`;
     return axiosClient.delete(url);
   },
 };
