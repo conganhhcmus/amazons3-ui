37b767eb0b936a58acff662fa1da9ce977938116
Phuc Hoang <hvphuc081299@gmail.com>
2021-06-16 11:21:46 +0700

    add user api

--- src/api/iamuserApi.ts
+++ src/api/iamuserApi.ts
@@ -1,23 +1,25 @@
 import { user } from 'app/userlist/userliststore';
 import axiosClient from './axiosClient';
 
+const baseUrl = 'https://user-service-s3.herokuapp.com/';
+
 const iamUserApi = {
   getBuckets: (): Promise<{ data: user[] }> => {
-    const url = 'api/v1/buckets';
+    const url = `${baseUrl}api/v1/buckets`;
     return axiosClient.get(url);
   },
-  createBucket: (userName: string, passWord: string,permisstion: number ): Promise<{ newIamuser: user }> => {
-    const url = 'api/v1/buckets';
+  createBucket: (userName: string, passWord: string, permisstion: number): Promise<{ newIamuser: user }> => {
+    const url = `${baseUrl}api/v1/buckets`;
     const body = {
       userName,
       passWord,
-      permisstion
+      permisstion,
     };
     return axiosClient.post(url, body);
   },
 
   deleteBucket: (id: string): Promise<{ success: boolean }> => {
-    const url = `api/v1/buckets/${id}`;
+    const url = `${baseUrl}api/v1/buckets/${id}`;
     return axiosClient.delete(url);
   },
 };
